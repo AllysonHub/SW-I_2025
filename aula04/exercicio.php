32b70f71deed455538dc99048829f4aaf5639c3f
Allyson <159627434+AllysonHub@users.noreply.github.com>
2025-03-06 11:08:54 -0300

    Add files via upload

---
+++ aula04/exercicio.php
@@ -0,0 +1,7 @@
+<?php
+    $n1 = $_GET['n1'];
+    $n2 = $_GET['n2'];
+    $n3 = $_GET['n3'];
+    $media = ($n1 + $n2 + $n3)/3;
+    echo "media $media";
+?>
---
+++ aula04/exercicio002.php
@@ -0,0 +1,6 @@
+<?php
+    $login = $_POST('login');
+    $pw = $_POST('pw');
+
+    if ($login) == ("etec") && 
+?>
