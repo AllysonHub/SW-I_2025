a02f0e0c28d7b5f1e9e6320e9e0cbd91d0afaf58
AllysonHub <allysonnrocha02@gmail.com>
2025-03-11 17:36:50 -0300

    css no exercicio post 02

--- aula04/exerciciosPOST/expost02.php
+++ aula04/exerciciosPOST/expost02.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 <?php 
     if ($_POST) {
         $nome = $_POST['nome'];
@@ -6,9 +15,11 @@
         $bandeira_cartao = $_POST['bandeira_cartao'];
 
         echo "<h2>Seus dados:</h2>";
-        echo "Nome: $nome <br>";
-        echo "Email: $email <br>";
-        echo "Data de Nascimento: $data_nasci <br>";
-        echo "Banderira Do Cartão: $bandeira_cartao <br>";
+        echo "<h3>Nome: $nome</h3> <br>";
+        echo "<h3>Email: $email</h3> <br>";
+        echo "<h3>Data de Nascimento: $data_nasci</h3> <br>";
+        echo "<h3>Banderira Do Cartão: $bandeira_cartao</h3> <br>";
     }
 ?>
+</body>
+</html>
