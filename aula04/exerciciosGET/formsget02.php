661c0d752e914a311211ab1b7e9e25a7ea61794e
AllysonHub <allysonnrocha02@gmail.com>
2025-03-11 16:19:22 -0300

    finalizado exercicios get

--- aula04/exerciciosGET/formsget02.php
+++ aula04/exerciciosGET/formsget02.php
@@ -6,6 +6,24 @@
     <title>Exercicio 02 com GET</title>
 </head>
 <body>
-    
+    <form action="exget02.php" method="GET">
+        <div>
+            <p>
+               usando o metodo GET, crie um formulario que receba tres numeros e verifique qual deles é o maior. <br> Faça a validaçao dos campos de modo que o usuario tenha que preencher todos os campos
+            </p>
+            <label for="n1">Numero 1: </label>
+            <input type="number" name="n1" id="idn1" required>
+        </div>
+        <div>
+            <label for="n2">Numero 2: </label>
+            <input type="number" name="n2" id="idn2" required>'
+        </div>
+        <div>
+            <label for="n3">Numero 3: </label>
+            <input type="number" name="n3" id="idn3" required>
+        </div>
+        <input type="submit" value="Enviar!">
+    </form>
+
 </body>
 </html>
